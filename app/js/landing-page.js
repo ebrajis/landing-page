352eefd5991ec125dcd0e905f8ad059ed1696ea5
ebrajis <brajis.evaldas@gmail.com>
2023-04-25 23:52:07 +0300

    updated

--- app/js/landing-page.js
+++ app/js/landing-page.js
@@ -1,13 +1,13 @@
 const navBar = document.querySelector('.header__nav');
 const mobHamburger = document.querySelector('.header__container__menu');
-///hamburger.
+///hamburger
 function toggleHamburger() {
   mobHamburger.classList.toggle('active');
   navBar.classList.toggle('active');
 }
 mobHamburger.addEventListener('click', toggleHamburger);
 
-///accordion.
+///accordion
 const contentBox = document.getElementsByClassName('section__faq__accordion__box');
 for (let i = 0; i < contentBox.length; i++) {
   contentBox[i].addEventListener('click', function () {
